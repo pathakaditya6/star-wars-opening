809395e2b4fd6d707dddec2c5613e8d8072fd84a
pathakaditya6 <pathakaditya6@protonmail.com>
2025-04-01 16:42:12 +0530

    Update project files and remove unnecessary assets

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 /*
  * Star Wars opening crawl from 1977
  * 
- * I freaking love Star Wars, but could not find
- * a web version of the original opening crawl from 1977.
+ * I freaking love Star Wars,
+ * A web version of Old original opening crawl.
  * So I created this one.
- *
- * I wrote an article where I explain how this works:
- * https://nerddis.co/star-wars-opening-crawl-from-1977
- * 
- * Watch the Start Wars opening crawl on YouTube.
- * https://www.youtube.com/watch?v=7jK-jZo6xjY
  * 
  * Stuff I used:
  * - CSS (animation, transform)
@@ -17,23 +11,16 @@
  * - SVG (the Star Wars logo from wikimedia.org)
  *   http://commons.wikimedia.org/wiki/File:Star_Wars_Logo.svg
  * - JavaScript (to sync the animation/audio)
- *
- * Thanks to Craig Buckler for his amazing article 
- * which helped me to create this remake of the Star Wars opening crawl. 
- * http://www.sitepoint.com/css3-starwars-scrolling-text/ 
- *
- * Sound copyright by The Walt Disney Company.
  * 
+ * Sound copyright by The Walt Disney Company.🤞😘🎶😁
  *
- * 2013 by Tim Pietrusky
- * nerddis.co
- * 
+ * 2025 by @Aditya Pathak
  */
+
+
 StarWars = (function() {
   
-  /* 
-   * Constructor
-   */
+  //Constructor
   function StarWars(args) {
     // Context wrapper
     this.el = $(args.el);
@@ -64,9 +51,7 @@ StarWars = (function() {
     }, this));
   }
   
-  /*
-   * Resets the animation and shows the start screen.
-   */
+  /*Resets the animation and shows the start screen.*/
   StarWars.prototype.reset = function() {
     this.start.show();
     this.cloned = this.animation.clone(true);
